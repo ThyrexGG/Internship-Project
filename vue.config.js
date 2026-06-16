c17709273aefa91952ef8db8cd5064170b777324
Thyrak <tesbonnathyrak@gmail.com>
2026-06-16 21:11:53 +0700

    .

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    https: false
+    https: true
   },
   configureWebpack: {
     resolve: {
